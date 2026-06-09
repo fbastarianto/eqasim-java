a0e5c518e02409b8726a95b64bb6c141f15226ed
Faza Bastarianto <faza.fawzan@ugm.ac.id>
2026-06-09 11:35:42 +0700

    - removed estimateAccessEgressTimeUtility(variables_car) for consistency with estimated DCM/R utility. - added b_td_car * (td_car_single / 1000) for consistency with estimated DCM/R utility.

--- jakarta/src/main/java/org/eqasim/jakarta/mode_choice/utilities/estimators/JakartaCarUtilityEstimator.java
+++ jakarta/src/main/java/org/eqasim/jakarta/mode_choice/utilities/estimators/JakartaCarUtilityEstimator.java
@@ -46,9 +46,19 @@ public class JakartaCarUtilityEstimator extends CarUtilityEstimator {
 		utility += estimateConstantUtility();
 		utility += estimateTravelTimeUtility(variables_car);
 		//utility += estimateRegionalUtility(variables);
-		utility += estimateAccessEgressTimeUtility(variables_car);
-		utility += estimateMonetaryCostUtility(variables_car) * EstimatorUtils.interaction(variables.hhlIncome, 
-				parameters.jAvgHHLIncome.avg_hhl_income, parameters.jIncomeElasticity.lambda_income);
+		// utility += estimateAccessEgressTimeUtility(variables_car); // removed for consistency with estimated DCM/R utility. // adds a penalty for that extra access/egress time, using a coefficient: parameters.car.additionalAccessEgressWalkTime_min
+
+		// b_tc_value * tc_car_single, with income elasticity
+		utility += estimateMonetaryCostUtility(variables_car) * EstimatorUtils.interaction(
+				variables.hhlIncome,
+				parameters.jAvgHHLIncome.avg_hhl_income,
+				parameters.jIncomeElasticity.lambda_income);
+
+		// b_td_car * (td_car_single / 1000)
+		utility += parameters.jCar.betaTravelDistance_km * variables_car.euclideanDistance_km;
+
+		// b_age_car * AGE
+		utility += parameters.jCar.alpha_age * variables.age;
 		
 		//if (variables.hhlIncome == 0.0)
 		//	utility += estimateMonetaryCostUtility(variables_car)
