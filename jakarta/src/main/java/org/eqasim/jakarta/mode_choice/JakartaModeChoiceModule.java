b07a64a47e974217966bab89014f08ef21c46c0e
Faza Bastarianto <faza.fawzan@ugm.ac.id>
2026-06-17 23:27:22 +0700

    register JakartaModeChoiceModule.java as a DMC TripConstraintFactory in JakartaModeChoiceModule.java

--- jakarta/src/main/java/org/eqasim/jakarta/mode_choice/JakartaModeChoiceModule.java
+++ jakarta/src/main/java/org/eqasim/jakarta/mode_choice/JakartaModeChoiceModule.java
@@ -11,6 +11,7 @@ import org.eqasim.core.simulation.mode_choice.cost.CostModel;
 import org.eqasim.core.simulation.mode_choice.parameters.ModeParameters;
 import org.eqasim.jakarta.mode_choice.constraints.VehicleTourConstraintWithCarPassenger;
 import org.eqasim.jakarta.mode_choice.constraints.WalkDurationConstraint;
+import org.eqasim.jakarta.mode_choice.constraints.NoMotorcycleEgressExceptHome;
 import org.eqasim.jakarta.mode_choice.costs.JakartaCarCostModel;
 import org.eqasim.jakarta.mode_choice.costs.JakartaCarodtCostModel;
 import org.eqasim.jakarta.mode_choice.costs.JakartaMcodtCostModel;
@@ -89,8 +90,8 @@ public class JakartaModeChoiceModule extends AbstractEqasimExtension {
 		.to(VehicleTourConstraintWithCarPassenger.Factory.class);
 		bindTripConstraintFactory("WalkDurationConstraint")
 		.to(WalkDurationConstraint.Factory.class);
-		//bindTripConstraintFactory("NoMotorcycleEgressExceptHome")
-		//		.to(NoMotorcycleEgressExceptHome.Factory.class);
+		bindTripConstraintFactory("NoMotorcycleEgressExceptHome")
+				.to(NoMotorcycleEgressExceptHome.Factory.class);
 
 		bindCostModel(CAR_COST_MODEL_NAME).to(JakartaCarCostModel.class);
 		bindCostModel(PT_COST_MODEL_NAME).to(JakartaPtCostModel.class);
